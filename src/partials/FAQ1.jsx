aee4c2ca35e40c006002f21f5bfe4a2435756163
ej838639 <eric.johnson838639@gmail.com>
2023-02-20 11:56:24 -0800

    feat: CHAR-1 Finished FAQ format

--- src/partials/FAQ1.jsx
+++ src/partials/FAQ1.jsx
@@ -17,7 +17,7 @@ function LearnMediation() {
           <div className="grid gap-20">
 
             {/* Text on left, image on right */}
-            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+            <div className="md:grid md:grid-cols-12 md:gap-6">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
                 <img className="max-w-full mx-auto md:max-w-none h-auto" src={Image01} width="540" height="405" alt="Features 01" />
@@ -25,27 +25,28 @@ function LearnMediation() {
               </div>
               {/* Text */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
-                <div className="md:pr-4 lg:pr-12 xl:pr-16">
-                  <h1 className="h4 mb-4">How is mediation different than counseling?</h1>
-                  <p className="text-xl text-gray-400 mb-4">Mediatorts work with two or more people. Counselors work with an individual.</p>
-                  <br></br>
-                  <h1 className="h4 mb-4">How is mediation different than arbitration?</h1>
-                  <p className="text-xl text-gray-400 mb-4">Mediators facilitate a conversation among parties so they can make decisions that are important to them. Arbitrators hear the evidence from both sides and make a binding decision.</p>
-                  <br></br>
-                  <h1 className="h4 mb-4">What if the person I'm in conflict with is not willing to try mediation?</h1>
-                  <p className="text-xl text-gray-400 mb-4">A mediation can only be done with all parties agree to try it.</p>
-                </div>
+                <details>
+                  <summary className="h4 pb-2">How is counseling different than mediation or coaching?</summary>
+                    <div className="pb-8">
+                      <p className="text-xl text-gray-400">Mediators work with two or more people. Counselors work with an individual.</p>
+                    </div>
+                </details>
+                <details>
+                  <summary className="h4 pb-2">Does Charis Mediation offer arbitration?</summary>
+                      <div className="pb-8">
+                        <p className="text-xl text-gray-400">Charis does not offer arbitration. Mediators facilitate a conversation among parties so they can make decisions that are important to them. Arbitrators hear the evidence from both sides and make a binding decision.</p>
+                      </div>
+                </details>
+                <details>
+                  <summary className="h4 pb-2">What if the person I'm in conflict with is not willing to try mediation?</summary>
+                      <div className="pb-8">
+                        <p className="text-xl text-gray-400">A mediation can only be done with all parties agree to try it.</p>
+                      </div>
+                </details>
               </div>
             </div>
 
           </div>
-
-          {/* Bottom section of text */}
-          <div className="max-w-6xl mx-auto text-left pb-12 md:pb-16">
-            <br></br>
-            <p className="text-xl text-gray-400">Mediation can be used in a variety of disputes including divorce, parenting plans, elder care, parent/teen issues, school conflicts among students, neighbor disputes, and church conflicts. It rebuilds relationships, is much faster than the court system, is less costly that litigation, and has a high settlement rate. Mediation allows you to settle conflicts in safe, confidential, and efficient manner. Questions you should consider before going into a mediation are: What is your purpose for having the conversation? What do you hope to accomplish? What is the ideal outcome? What assumptions are you making about the person's intentions? What "buttons" of yours are being pushed? How is your attitude toward the conversations influencing your perception of it? How does the other party perceive the problem? What are their needs and fears? What solution would they suggest? How have you contributed to the problem? How have they?</p>
-          </div>
-
         </div>
       </div>
     </section>
